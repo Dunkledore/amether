5b2e1aec56dcecfe3d9e920b5616fe417af31824
Dunkledore <b7dunkerley@me.com>
2018-10-16 17:03:56 +0200

    change to flask

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from quart import Quart, request, flash, redirect, url_for, send_from_directory
+from quart import Flask, request, flash, redirect, url_for, send_from_directory
 import os
 import string
 import random
@@ -8,7 +8,7 @@ UPLOAD_FOLDER = 'chats'
 ALLOWED_EXTENSIONS = {"zip"}
 
 
-app = Quart(__name__)
+app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
@@ -16,14 +16,14 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
-async def upload_file():
+def upload_file():
     print(request.method)
     if request.method == 'POST':
         # check if the post request has the file part
-        if 'file' not in await request.files:
+        if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
-        file = (await request.files)['file']
+        file = (request.files)['file']
         # if user does not select file, browser also
         # submit an empty part without filename
         if file.filename == '':
@@ -46,8 +46,8 @@ async def upload_file():
     '''
 
 @app.route('/outputs/<filename>')
-async def uploaded_file(filename):
-    return await send_from_directory("outputs", f"{filename}.png")
+def uploaded_file(filename):
+    return send_from_directory("outputs", f"{filename}.png")
 
 
 
